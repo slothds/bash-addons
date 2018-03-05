375f515bf15a199a281c250cba2e6cda1f500b2d
Sloth DevilS <sloth@devils.su>
2018-03-05 11:31:33 +0300

    history hack

--- bash_history.sh
+++ bash_history.sh
@@ -1,13 +1,22 @@
-export HISTCONTROL=ignoredups:erasedups  # no duplicate entries
-export HISTSIZE=100000                   # big big history
-export HISTFILESIZE=100000               # big big history
+history_hack() {
+    export HISTCONTROL=ignoredups:erasedups  # no duplicate entries
+    export HISTSIZE=100000                   # big big history
+    export HISTFILESIZE=100000               # big big history
+    case ${PROMPT_COMMAND} in
+        *'history -a'*) ;;
+        *)                export PROMPT_COMMAND="history -a;${PROMPT_COMMAND}" ;;
+    esac
+}
+
 case ${PROMPT_COMMAND} in
-    *'history'*) ;;
-    *)         export PROMPT_COMMAND="history -a;${PROMPT_COMMAND}" ;;
+    *'history_hack'*) ;;
+    *)                export PROMPT_COMMAND="history_hack;${PROMPT_COMMAND}" ;;
 esac
 
 # append to history, don't overwrite it
-shopt -s histappend
+if [ "x`shopt | awk '/histappend/{print $2}'`" != "xon" ];then
+    shopt -s histappend
+fi
 
 # Search through history by arrows up\down
 bind '"\e[A": history-search-backward'
