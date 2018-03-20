cb0cd76ddc975147aa9a6df2c4444089e830edd3
Sloth <sloth@devils.su>
2018-03-20 10:37:52 +0300

    New Prompt generator function * dot.bash_prompt.example -> dot.bash_prompt; * added prompt examples blank; * added custom strings and colors support;

--- bash-prompt/bash_prompt.sh
+++ bash-prompt/bash_prompt.sh
@@ -1,11 +1,9 @@
 if [ -n "$BASH_VERSION" -a -n "$PS1" ]; then
     __bash_prompt_build() {
-        local user_string path_string char_string time_string git_string
-        local cc cu cw cp ct cg
-        local user_color path_color char_color time_color git_color
-        local time_enable git_enable
-        local _user _path _char _time _git
-        local prompt_default prompt_string
+        local string_list string_user string_path string_char string_time string_git
+        local color_list color_user color_path color_char color_time color_git
+        local color_enable time_enable git_enable
+        local prompt_default prompt
 
         if [ -f "${HOME}/.bash_prompt" ];then
             source ${HOME}/.bash_prompt
@@ -13,50 +11,76 @@ if [ -n "$BASH_VERSION" -a -n "$PS1" ]; then
 
         color_enable=${color_enable:-yes}
 
-        if [ -n "${color_enable}" -a "x${color_enable}" = "xyes" ];then
-            cc="\[\e[0m\]"
-            [ `id -u` -gt 0 ] && cu="\[\e[${user_color:-0;32}m\]" || cu="\[\e[0;31m\]"
-            cw="\[\e[${path_color:-3;33}m\]"
-            cp="\[\e[${char_color:-2;33}m\]"
-            ct="\[\e[${time_color:-0;00}m\]"
-            cg="\[\e[${git_color:-6;36}m\]"
-        else
-            unset cc cu cw cp ct cg
-        fi
-        user_string="${user_string:-\\u}"
-        path_string="${path_string:-\\W}"
-        char_string="${char_string:- \\$}"
+        color_user=${color_user:-0;32}
+        color_path=${color_path:-3;33}
+        color_char=${color_char:-2;33}
+        color_time=${color_time:-0;00}
+        color_git=${color_git:-6;36}
 
-        _user="${cu}${user_string}${cc}"
-        _path="${cw}${path_string}${cc}"
-        _char="${cp}${char_string}${cc}"
-        if [ -n "${time_enable}" -a "x${time_enable}" = "xyes" ];then
-            time_string="${time_string:-\\A }"
-            _time="${ct}${time_string}${cc}"
+        string_user=${string_user:-\\u}
+        string_path=${string_path:-\\w}
+        string_char=${string_char:-\\$}
+        string_time=${string_time:-\\A }
+        string_git=${string_git:- (%s)}
+
+        color_list=`compgen -v | grep -P '^color_.+$' | tr '\n' ' '`
+
+        if [ -n "${color_enable}" -a "x${color_enable}" = "xyes" ];then
+            REGEX='^[0-9]{1};[0-9]{2,3}$'
+            for color in ${color_list};do
+                if [[ ${!color} =~ ${REGEX} ]];then
+                    eval ${color}='`printf "\\\\\\[\\\\\\e[%sm\\\\\\]" "${!color}"`'
+                fi
+            done
+            unset color
+
+            color_close="\[\e[0m\]"
+            [ `id -u` -eq 0 ] && color_user="\[\e[0;31m\]" || true
+        else
+            unset ${color_list} color_list
+        fi
+
+        string_list=`compgen -v | grep -P '^string_.+$' | tr '\n' ' '`
+
+        for string in ${string_list};do
+            part=`echo ${string}|awk -F'_' '{print $2}'`
+            eval color=\${color_${part}}
+            if [ -n "${color}" -a "${color}" != "-" ];then
+                eval ${string}="'${color}${!string}${color_close}'"
+            else
+                eval ${string}="\${!string}"
+            fi
+            unset part color
+        done
+        unset string
+
+        if [ -n "${time_enable}" -a "x${time_enable}" != "xyes" ];then
+            unset string_time
         fi
         if [ -n "${git_enable}" -a "x${git_enable}" = "xyes" ];then
             if [ -f /etc/profile.d/git-prompt.sh ];then
                 source /etc/profile.d/git-prompt.sh
-                git_string="${git_string:- (%s)}"
-                _git="${cg}`__git_ps1 "${git_string}"`${cc}"
+                string_git=`__git_ps1 "${string_git}"`
+            else
+                unset string_git
             fi
-        fi
-
-        prompt_default="${_time}[ ${_user} ${_path}${_git} ]${_char}"
-        if [ -z "${prompt_string}" ];then
-            prompt_string="${prompt_default}"
         else
-            prompt_string=`eval echo "${prompt_string}"`
+            unset string_git
         fi
 
-        export PS1=" ${prompt_string} "
+        prompt_default="${string_time}[ ${string_user} ${string_path}${string_git} ] ${string_char}"
+        if [ -z "${prompt}" ];then
+            prompt="${prompt_default}"
+        else
+            prompt=`eval echo "${prompt}"`
+        fi
 
-        unset user_string path_string char_string time_string git_string
-        unset cc cu cw cp ct cg
-        unset user_color path_color char_color time_color git_color
-        unset time_enable git_enable
-        unset _user _path _char _time _git
-        unset prompt_default prompt_string
+        export PS1=" ${prompt} "
+
+        unset string_list ${string_list}
+        unset color_list color_close ${color_list}
+        unset color_enable time_enable git_enable
+        unset prompt_default prompt
     }
 
     case ${PROMPT_COMMAND} in
